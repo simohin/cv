74cb18e9ff96a54ddb333ec99ecb49cf7f42ae44
Timofei_Simohin <timofei_simohin@epam.com>
2022-01-10 22:16:45 +0500

    calendar

--- src/main/java/com/simohin/cv/frontend/view/main/CalendarView.java
+++ src/main/java/com/simohin/cv/frontend/view/main/CalendarView.java
@@ -1,6 +1,8 @@
 package com.simohin.cv.frontend.view.main;
 
 import com.simohin.cv.frontend.view.View;
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.html.IFrame;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,22 @@ import org.springframework.stereotype.Component;
 public class CalendarView extends VerticalLayout implements View {
 
     protected static final String COMPONENT_NAME = "Calendar";
+
+    public CalendarView() {
+        setSizeFull();
+        add(getCalendlyIframe());
+    }
+
+    private IFrame getCalendlyIframe() {
+        return new IFrame() {{
+            setPadding(false);
+            setMargin(false);
+            getStyle().set("overflow", "hidden");
+            getElement().setAttribute("frameborder", "0").setAttribute("scrolling", "no");
+            setSrc("https://calendly.com/simohin_timofei/30min?embed_type=Inline&hide_gdpr_banner=1");
+            setSizeFull();
+        }};
+    }
 
     @Override
     public String getName() {
