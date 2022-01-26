74d8ecf31fdef94711cb1cbc86142d455d739a57
Timofei_Simohin <timofei_simohin@epam.com>
2022-01-26 09:19:22 +0500

    dark mode

---
+++ src/main/java/com/simohin/cv/frontend/component/TimeLineItems.java
@@ -0,0 +1,29 @@
+package com.simohin.cv.frontend.component;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Collection;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class TimeLineItems extends VerticalLayout {
+
+    private Component[] items;
+
+    public TimeLineItems(Collection<TimeLineItem> items) {
+        setItems(items);
+        setMaxWidth(90f, Unit.VH);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setAlignSelf(Alignment.CENTER, this);
+        setAlignItems(Alignment.CENTER);
+        add(this.items);
+    }
+
+    public void setItems(Collection<TimeLineItem> items) {
+        this.items = items.toArray(new Component[0]);
+    }
+}
